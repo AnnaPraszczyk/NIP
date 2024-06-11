2a77edb0be03dcc4da8cdfcb463fdc6728c9131b
pleanna <74422085+pleanna@users.noreply.github.com>
2024-06-11 18:26:03 +0200

    Dodaj pliki projektów.

---
+++ NIP2.Blazor.Server/BlazorApplication.cs
@@ -0,0 +1,47 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Xpo;
+using NIP2.Blazor.Server.Services;
+
+namespace NIP2.Blazor.Server;
+
+public class NIP2BlazorApplication : BlazorApplication {
+    public NIP2BlazorApplication() {
+        ApplicationName = "NIP2";
+        CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+        DatabaseVersionMismatch += NIP2BlazorApplication_DatabaseVersionMismatch;
+    }
+    protected override void OnSetupStarted() {
+        base.OnSetupStarted();
+#if DEBUG
+        if(System.Diagnostics.Debugger.IsAttached && CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+            DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+        }
+#endif
+    }
+    private void NIP2BlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+        e.Updater.Update();
+        e.Handled = true;
+#else
+        if(System.Diagnostics.Debugger.IsAttached) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+        else {
+            string message = "The application cannot connect to the specified database, " +
+                "because the database doesn't exist, its version is older " +
+                "than that of the application or its schema does not match " +
+                "the ORM data model structure. To avoid this error, use one " +
+                "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+            if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+                message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+            }
+            throw new InvalidOperationException(message);
+        }
+#endif
+    }
+}
---
+++ NIP2.Module/BusinessObjects/PobieranieDanych.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+//namespace SimpleProjectManager.Module.BusinessObjects.Marketing
+namespace NIP2.Module.BusinessObjects.PobieranieDanych
+{
+
+    [NavigationItem("Pobieranie Danych")]
+    public class Customer : BaseObject
+    {
+        public Customer(Session session) : base(session) { }
+        string nip;
+        public string NIP
+        {
+            get { return nip; }
+            set { SetPropertyValue(nameof(NIP), ref nip, value); }
+        }
+        string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set { SetPropertyValue(nameof(FirstName), ref firstName, value); }
+        }
+        string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set { SetPropertyValue(nameof(LastName), ref lastName, value); }
+        }
+        string email;
+        public string Email
+        {
+            get { return email; }
+            set { SetPropertyValue(nameof(Email), ref email, value); }
+        }
+        string company;
+        public string Company
+        {
+            get { return company; }
+            set { SetPropertyValue(nameof(Company), ref company, value); }
+        }
+        string street;
+        public string Street
+        {
+            get { return street; }
+            set { SetPropertyValue(nameof(Street), ref street, value); }
+        }
+
+    }
+        /*[Association("Customer-Testimonials")]
+        public XPCollection<Testimonial> Testimonials
+        {
+            get { return GetCollection<Testimonial>(nameof(Testimonials)); }
+        }
+        public string FullName
+        {
+            get
+            {
+                string namePart = string.Format("{0} {1}", FirstName, LastName);
+                return Company != null ? string.Format("{0} ({1})", namePart, Company) : namePart;
+            }
+        }
+    }*/
+    [NavigationItem("Pobieranie Danych")]
+    public class DaneGUS : BaseObject
+    {
+        public DaneGUS(Session session) : base(session) { }
+        
+        string nipGUS;
+        public string NIPGUS
+        {
+            get { return nipGUS; }
+            set { SetPropertyValue(nameof(NIPGUS), ref nipGUS, value); }
+        }
+        string firstNameGUS;
+        public string FirstNameGUS
+        {
+            get { return firstNameGUS; }
+            set { SetPropertyValue(nameof(FirstNameGUS), ref firstNameGUS, value); }
+        }
+        string lastNameGUS;
+        public string LastNameGUS
+        {
+            get { return lastNameGUS; }
+            set { SetPropertyValue(nameof(LastNameGUS), ref lastNameGUS, value); }
+        }
+        string emailGUS;
+        public string EmailGUS
+        {
+            get { return emailGUS; }
+            set { SetPropertyValue(nameof(EmailGUS), ref emailGUS, value); }
+        }
+        string companyGUS;
+        public string CompanyGUS
+        {
+            get { return companyGUS; }
+            set { SetPropertyValue(nameof(CompanyGUS), ref companyGUS, value); }
+        }
+        string streetGUS;
+        public string StreetGUS
+        {
+            get { return streetGUS; }
+            set { SetPropertyValue(nameof(StreetGUS), ref streetGUS, value); }
+        }
+    }
+}
